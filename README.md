418a7787bd1944857f7f49e4ac308509078b82d3
Srujam Dave <srujamdave@gmail.com>
2024-03-28 00:16:24 -0700

    Added reset on mouse click, commented code, and wrote README file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gravitySimulation
+
+## About the project
+This project uses python to create a simple simulation of Newtonian gravity between planets.
+
+### Created Using:
+ * Python (including the numpy and tkinter libraries)
+
+In order to run the simulation, both numpy and tkinter must be installed. 
+
+The simulation runs automatically, and can be restarted by clicking the screen
--- gravity.py
+++ gravity.py
@@ -3,12 +3,14 @@ import math
 import numpy as np
 from random import randint
 
-
+#GLOBAL VARIABLES
 GRAVITY = 0.25
 DELAY = 10
 
-#
+#Contains methods for drawing each planet, updating its position, velocity,
+#and acceleration, and dealing with collisions between planets
 class Planet:
+    #Initializes each planet (and draws it with the self.ball attribute)
     def __init__(self, position, velocity, mass):
         self.position = position
         self.velocity = velocity
@@ -20,10 +22,12 @@ class Planet:
             self.position[1] - self.radius, self.position[0] + self.radius, 
             self.position[1] + self.radius, fill="black")
 
+    #Updates position and velocity each frame
     def UpdatePlanet(self):
         self.position = np.add(self.velocity, self.position)
         self.velocity = np.add(self.acceleration, self.velocity)
 
+    #Calculate the force of gravity from other planets on this planet
     def ApplyForces(self, planets):
         total_force = np.array([0, 0])
         for planet in planets:
@@ -38,21 +42,29 @@ class Planet:
 
         self.acceleration = total_force / self.mass
 
+    #If there's a collision between two planets, deletes both planets and
+    #creates a new one with the mass of both combined
     def CheckCollisions(self, planets):
+        #Iterates through every other planet and checks for collisions
         for planet in planets:
             if planet != self:
                 dist_vector = planet.position - self.position
                 distance = math.sqrt(dist_vector[0] ** 2
                     + dist_vector[1] ** 2)
-    
+                
                 if distance < (self.radius + planet.radius):
+                    #Position calculated by linearly interpolating between the
+                    #positions of the previous two planets based on their 
+                    #masses
                     new_pos = self.position + (dist_vector
                         * planet.mass / (planet.mass + self.mass))
                     new_mass = self.mass + planet.mass
+                    #velocity calculated through conservation of momentum
                     new_vel = (self.velocity * self.mass
                         + planet.velocity * planet.mass) / new_mass
                     new_planet = Planet(new_pos, new_vel, new_mass)
                     
+                    #Delete old two planets, their images, and add new one
                     planet_canvas.delete(planet.ball)
                     planet_canvas.delete(self.ball)
                     planets.remove(planet)
@@ -60,11 +72,12 @@ class Planet:
                         if planets[i] is self:
                             planets.remove(planets[i])
                             break
-
                     planets.append(new_planet)
 
 
 
+#Uses recursion to update each planet's attributes, move it, and check for 
+#collisions, then go to the next frame
 def move_planets():
     for planet in planets:
         planet.ApplyForces(planets)
@@ -75,15 +88,29 @@ def move_planets():
     planet_canvas.after(DELAY, move_planets)
 
 
-root = Tk()
-planet_canvas = Canvas(root, bg="white", height=1000, width=1000)
+#Populate array (and screen) with a bunch of planet objects
+def create_planets():
+    planets.clear()
+    planet_canvas.delete("all")
 
-planets = []
-
-for i in range(0, 40):
-    planets.append(Planet(np.array([randint(0, 1000), randint(0, 1000)]),
+    for i in range(0, randint(30, 50)):
+        planets.append(Planet(np.array([randint(0, 1000), randint(0, 1000)]),
         np.array([randint(-1, 1), randint(-1, 1)]), randint(50, 200)))
 
+#Create window and canvas
+root = Tk()
+planet_canvas = Canvas(root, bg="white", height=1000, width=1000)
 planet_canvas.pack()
+
+#populates window with lots of small planets
+planets = []
+create_planets()
+
+#Resets simulation if mouse pressed
+def reset_simulation(self):
+    create_planets()
+
+root.bind("<ButtonRelease>", reset_simulation)
+
 planet_canvas.after(DELAY, move_planets)
 root.mainloop()
